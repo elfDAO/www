db6c134537dd25ea883102eceefc02a25490033f
Byeongjun Moon <byeongjun@bjmoon.io>
2021-12-13 09:26:29 -0800

    minor css + content changes

--- components/Meta.js
+++ components/Meta.js
@@ -3,32 +3,32 @@ import Head from "next/head";
 const Meta = () => {
   return (
     <Head>
-      <title>ElfDAO</title>
-      <meta name="title" content="ElfDAO" />
+      <title>elfDAO</title>
+      <meta name="title" content="elfDAO" />
       <meta
         name="description"
-        content="Here at elfDAO, we are elves - the workers, builders, and contributors of holiday cheer. That's why we've created elfDAO - to fund, organize, and donate gifts to institutions, orphanages, and low-income neighborhood centers and bring holiday joy to as many children as possible."
+        content="This holiday season, we elves at elfDAO are on a mission to get gifts to kids in need."
       />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://www.elfdao.com/" />
-      <meta property="og:title" content="ElfDAO" />
+      <meta property="og:url" content="https://www.elfDAO.com/" />
+      <meta property="og:title" content="elfDAO" />
       <meta
         property="og:description"
-        content="Here at elfDAO, we are elves - the workers, builders, and contributors of holiday cheer. That's why we've created elfDAO - to fund, organize, and donate gifts to institutions, orphanages, and low-income neighborhood centers and bring holiday joy to as many children as possible."
+        content="This holiday season, we elves at elfDAO are on a mission to get gifts to kids in need."
       />
       <meta property="og:image" content="/meta_image.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://www.elfdao.com/" />
-      <meta property="twitter:title" content="ElfDAO" />
+      <meta property="twitter:url" content="https://www.elfDAO.com/" />
+      <meta property="twitter:title" content="elfDAO" />
       <meta
         property="twitter:description"
-        content="Here at elfDAO, we are elves - the workers, builders, and contributors of holiday cheer. That's why we've created elfDAO - to fund, organize, and donate gifts to institutions, orphanages, and low-income neighborhood centers and bring holiday joy to as many children as possible."
+        content="This holiday season, we elves at elfDAO are on a mission to get gifts to kids in need."
       />
       <meta
         property="twitter:image"
-        content="https://www.elfdao.com/meta_image.png"
+        content="https://www.elfDAO.com/meta_image.png"
       ></meta>
     </Head>
   );
