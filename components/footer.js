f1dc71b5756fca7e140ea2580a25dba365879661
straightupjac <jdc.jaclyn@gmail.com>
2021-12-18 20:56:52 -0600

    bottom nav

---
+++ components/footer.js
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Icon, Stack } from '@mui/material';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+export default function Footer() {
+
+  return (
+    <>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        paddingBottom={'1rem'}
+      >
+        <>
+          <a
+            href="https://twitter.com/elf_DAO"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TwitterIcon color="secondary" />
+          </a>
+        </>
+        <>
+        <a
+          href="https://discord.gg/elfdao"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image alt="discord logo" src="/discord.svg" width="25" height="25"/>
+        </a>
+        </>
+      </Stack>
+    </>
+  )
+}
+
+export function getStaticProps({ locale }) {
+  return {
+    props: {
+      messages: require(`../locales/${locale}.json`),
+    },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,65 @@ import Progress from "../components/Progress";
 import Nft from "../components/NftCard";
 import Grid from '@mui/material/Grid';
 import styled from 'styled-components';
+import Footer from "@components/footer";
 
 export default function Home() {
   const t = useTranslations();
 
   return (
-    <main>
-      <Meta />
-      <Navigation />
-      <header>
-        <h1 className="masthead">
-          {t.rich('home.tagline', { br: () => <br />})}
-        </h1>
-        <h2 style={{paddingBottom: '1.5rem', color: '#36ECAC', textAlign:"center"}}>{t('home.tldr')}</h2>
-        <Progress />
-      </header>
-      <article>
-        <p className="manifesto center" style={{marginBottom: '2rem'}}>
-          {t.rich('home.manifesto', {
-            strong: (children) => <strong>{children} </strong>,
-          })}
-        </p>
-        <p className="manifesto center">
-          {t.rich('home.why', {
-            strong: (children) => <strong>{children} </strong>,
-          })}
-        </p>
-      </article>
-      <Spacer />
-      <Grid container width="100%" spacing={3} direction="row" justifyContent="center">
-        <Grid item>
-          <Nft
-            name={t('nft.elf')}
-            value={'Contributions > 0.1 ETH'}
-            image={'/elf.svg'}
-            claimable={true}
-          />
+    <>
+      <main>
+        <Meta />
+        <Navigation />
+        <header>
+          <h1 className="masthead">
+            {t.rich('home.tagline', { br: () => <br />})}
+          </h1>
+          <h2 style={{paddingBottom: '1.5rem', color: '#36ECAC', textAlign:"center"}}>{t('home.tldr')}</h2>
+          <Progress />
+        </header>
+        <article>
+          <p className="manifesto center" style={{marginBottom: '2rem'}}>
+            {t.rich('home.manifesto', {
+              strong: (children) => <strong>{children} </strong>,
+            })}
+          </p>
+          <p className="manifesto center">
+            {t.rich('home.why', {
+              strong: (children) => <strong>{children} </strong>,
+            })}
+          </p>
+        </article>
+        <Spacer />
+        <Grid container width="100%" spacing={3} direction="row" justifyContent="center">
+          <Grid item>
+            <Nft
+              name={t('nft.elf')}
+              value={'Contributions > 0.1 ETH'}
+              image={'/elf.svg'}
+              claimable={true}
+            />
+          </Grid>
+          <Grid item>
+            <Nft
+              name={t('nft.reindeer')}
+              value={'Contributions > 0.5 ETH'}
+              image={'/reindeer.svg'}
+              claimable={false}
+            />
+          </Grid>
+          <Grid item>
+            <Nft
+              name={t('nft.santa')}
+              value={'Top 5 Contributers'}
+              image={'/santa.svg'}
+              claimable={false}
+            />
+          </Grid>
         </Grid>
-        <Grid item>
-          <Nft
-            name={t('nft.reindeer')}
-            value={'Contributions > 0.5 ETH'}
-            image={'/reindeer.svg'}
-            claimable={false}
-          />
-        </Grid>
-        <Grid item>
-          <Nft
-            name={t('nft.santa')}
-            value={'Top 5 Contributers'}
-            image={'/santa.svg'}
-            claimable={false}
-          />
-        </Grid>
-      </Grid>
-    </main>
+      </main>
+      <Footer />
+    </>
   );
 }
 
