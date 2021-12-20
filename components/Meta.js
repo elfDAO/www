91894a5c1b03599d0e27d09bab493b8cfb47b938
straightupjac <jdc.jaclyn@gmail.com>
2021-12-20 13:12:50 -0600

    update twitter title

--- components/Meta.js
+++ components/Meta.js
@@ -21,11 +21,7 @@ const Meta = () => {
 
       <meta property="twitter:card" content="/website-preview.png" />
       <meta property="twitter:url" content="https://twitter.com/elf_DAO" />
-      <meta property="twitter:title" content="elfDAO" />
-      <meta
-        property="twitter:description"
-        content="This holiday season, we elves at elfDAO are on a mission to get gifts to kids in need."
-      />
+      <meta property="twitter:title" content="elfDAO. WAGBG." />
       <meta
         property="twitter:image"
         content="/website-preview.png"
